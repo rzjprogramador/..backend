551e2e96bae5e054b1a9625ab6a72cd6cb2281a4
rzjprogramador <rzjprogramador@gmail.com>
2023-02-07 22:40:14 -0300

    refactor: okok

--- dotnet/BaseV7/conceitos/Interpolacao_Concatenacao.cs
+++ dotnet/BaseV7/conceitos/Interpolacao_Concatenacao.cs
@@ -16,12 +16,18 @@ public class Interpolacao {
     // "PULEI VAI VIR OUTRO MAIS APOS ASPAS >> " +
     // "PULEI A TERCEIRA E CHEGA !!!";
 
-    var interpolada_ou_concatenada = $"""
-    inserido 3 aspas duplas e posso pular de linha avontade esta é a linha1
-    ESTA É MINHA VAR INTERPOLADA >> {variavel}
-    linha 2 aqui
-    linha 3 aqui
-    chega !!!
+    // var interpolada_ou_concatenada = $"""
+    // inserido 3 aspas duplas e posso pular de linha avontade esta é a linha1
+    // ESTA É MINHA VAR INTERPOLADA >> {variavel}
+    // linha 2 aqui
+    // linha 3 aqui
+    // chega !!!
+    // """;
+
+    var interpolada_ou_concatenada = $$"""
+    escapar template json escapando chaves e interpolacao ao copiar para a string o json :
+    solucao usar 2 $$ dolar ants das aspas  e 2  na chaves nal interpolada onde tem json na string
+      exemplo de json aqui na string  {"chave": "valor" } ele sozinho nao consegue escapar as chaves ...este foi um json ou objetoLiteral esta é uma var agora cque tem json {{varNovaOndeTemJson}}
     """;
 
     return interpolada_ou_concatenada;
