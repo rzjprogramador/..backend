d9cb0ccf21027a5bd037ac21333fd1dd7894986f
rzjprogramador <rzjprogramador@gmail.com>
2023-02-10 16:25:44 -0300

    feat: funcao ler arquivos de forma recursiva

--- dotnet/BaseV7/Program.cs
+++ dotnet/BaseV7/Program.cs
@@ -1,9 +1,16 @@
 ﻿using funcao;
+using recursivas;
+
 namespace principal;
 
 public class Program {
   public static void Main(string[] args) {
-    Console.WriteLine(ClasseFuncao.FuncaoSomaDeInteiros());
+    Recursiva.LerArquivo(1);
+    // Console.WriteLine(Recursiva.LerArquivo());
+    // Console.WriteLine(ClasseFuncao.FuncaoSomaDeInteiros());
   }
 }
 
+/*
+o @ arroba antes da string é para escapar as barras e nao dar incompatibilidade.
+*/
