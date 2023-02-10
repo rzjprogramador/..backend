bff406fd27ce199a635ef19660695e6399ec8f58
rzjprogramador <rzjprogramador@gmail.com>
2023-02-10 08:58:31 -0300

    refactor: okok

--- dotnet/BaseV7/Program.cs
+++ dotnet/BaseV7/Program.cs
@@ -1,25 +1,11 @@
-﻿// Este arquivo ja é a classe principal executando o metodo Main
-
-// using Cliente;
-// using System; 
+﻿
 
 namespace principal;
 
 public class Program {
   public static void Main(string[] args) {
 
-    // seguranca recebimento de dados
-    string campo1 = new segurancaRecebimentoValor.CamposDeClasse("foo");
-    Console.WriteLine(campo1);
-
-    // metodos de tipos Textos :
-    // metodosDeTipos.MetodosDeTextos.Execute();
-
-    // Console.WriteLine(formatacao.Interpolacao.Execute());
-
-  // Console.WriteLine(modulo.ClasseModulo1.Execute1());
-  // Console.WriteLine(modulo.ClasseModulo2.Execute2());
-  // Console.WriteLine("Oi");
+    
   }
 }
 
--- dotnet/BaseV7/Seguranca_Recebimento_Valor/Campos_De_Classe.cs
+++ dotnet/BaseV7/Seguranca_Recebimento_Valor/Campos_De_Classe.cs
@@ -1,10 +1,12 @@
 namespace segurancaRecebimentoValor;
 
-public static CamposDeClasse 
+public class CamposDeClasse 
 {
   public string campo_NaoPodeSerNulo { get; set; } = default!; // este campo esta seguro que nao pode receber nada/nulo
 
-  public string? campo_PodeSerNulo ; // mostra no tipo que pode receber nulo - entao tenho que verifica-lo ou dar um valor padrao pra ele se vier a vir sem nada/nulo
+  public string? campo_PodeSerNulo = default! ; // mostra no tipo que pode receber nulo - entao tenho que verifica-lo ou dar um valor padrao pra ele se vier a vir sem nada/nulo
+
+  public required string campoObrigatorioNaInstanciacao { get; set; } // required torna este campo obrigatorio na instanciacao da classe
 
   // #TODO : fazer constructor em c#
 
