643860b8a38634f3450722219b8c71863c0500d5
rzjprogramador <rzjprogramador@gmail.com>
2023-02-16 15:50:34 -0300

    refactor: tipos de dados interfaceQualquer em golang

--- golang/02_tipos_de_dados/Tipo_Qualquer_Interface_TipoAny_Inseguro_Livre.go
+++ golang/02_tipos_de_dados/Tipo_Qualquer_Interface_TipoAny_Inseguro_Livre.go
@@ -12,18 +12,17 @@ func main() {
 }
 
 /*
-Nome: "interface qulquerTipo",
-
-Keyword: "interface{}",
-
-Deixa_Variavel_Receber: "Qualquer tipo de dado."
-
-Pode_Reescrever_Valor_Da_Variavel: true,
-
-Uso: "interface{} // na declaracao"
-
-Conceito: interface{} é semelhante ao tipo any de outras linguagens, aceita qualquer mudanca de tipo e valor na variavel.
-
-Aplicabilidade: é um tipo inseguro, só use quando realmente nao sabe o que pode vir para esta variavel ex: em recebimentos especificos http.
+QualquerInterface: ModelTipos{
+		Linguagem: "Golang",
+		Nome: "QualquerInterface",
+		Keyword: "interface{}",
+		Representacao_PodeInstanciar: "qualquer tipo de valor é livre",
+		Conceito: "é semelhante ao tipo any de outras linguagens, aceita qualquer mudanca de tipo e valor na variavel.",
+		Pode_Reescrever_Valor_Da_Variavel: true,
+		Uso: "",
+		Aplicabilidade: "é um tipo inseguro, só use quando realmente nao sabe o que pode vir para esta variavel ex: em recebimentos especificos http.",
+		ValorDefault_SeNadaForPassado: "nil // nulo - NaoTemNadaDeValor",
+		Exemplo: "``` var foo interface{} ```",
+	},
 
 */
