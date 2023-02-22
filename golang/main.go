bb81aba779978f9df26c3a79cf3a8e0790319b85
rzjprogramador <rzjprogramador@gmail.com>
2023-02-22 15:59:53 -0300

    refactor: okok

--- golang/main.go
+++ golang/main.go
@@ -3,16 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// tentando :: Template Literals com Interpolacao de String
-	primeira := "primeiro texto"
-	pulaLinhas := `Linha1 
-	Linha2
-	Linha3`
-
-	publicar := fmt.Sprintf("%#v %#v", primeira, pulaLinhas, )
-	// #TODO : devolveu 2 strings : "primeiro texto" "Linha1\n\tLinha2\n\tLinha3"
-	// fmt.Println(publicar)
-
-	
-
+	// 
+	// publicar := "{ } # $ % @ ! & * ^ (  ) [ ] + × ÷ = / _ € £ ¥ ₩ , : ; ? ."
+	fmt.Println("{ } # $ % @ ! & * ^ (  ) [ ] + × ÷ = / _ € £ ¥ ₩ , : ; ? .")
 }
